435d966545c1a2c06d9ebcfe3d9115ba7c57de85
Jonathan Saewitz <busterroni11@gmail.com>
2015-11-18 11:15:38 -0500

    add new methods
    add getName() and getSchoolName() methods, rename the previous getSchoolName() method to the more accurate getSchoolDistrictName()

--- powerapi/user.py
+++ powerapi/user.py
@@ -17,8 +17,29 @@ class user:
 		
 		return courses
 	
-	def getSchoolName(self):
+	def getName(self):
+		#get the name in format "Last, First Middle" from the page's html
+		name = re.search(r'<h1>Grades and Attendance: (.*?)</h1>', self.homeContents, re.S).groups()[0].strip()
+		
+		#get the last name
+		lastName = re.search(r'(.*), ', name).groups()[0].strip()
+		
+		#get the first name by removing the last name from the 'name' string
+		firstName = re.sub(r'(.*), ', '', name)
+		
+		#concatenate the first and last names
+		name = firstName + " " + lastName
+		
+		#return the name in the format "First Middle Last"
+		return name
+	
+	def getSchoolDistrictName(self):
 		name = re.search(r'<div id="print-school">(.*?)<br>', self.homeContents, re.S)
+		
+		return name.groups()[0].strip()
+		
+	def getSchoolName(self):
+		name = re.search(r'<div id="print-school">.*?<br><span>(.*?)</span></div>', self.homeContents, re.S)
 		
 		return name.groups()[0].strip()
 	
